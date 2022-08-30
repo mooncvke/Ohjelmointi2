af7a071634be9fff36668a9b59259bc15003d8dd
Tuuli Silvennoinen <tuuli.silvennoinen@tuni.fi>
2022-08-30 15:43:57 +0300

    fixed typos

--- student/02/temperature/main.cpp
+++ student/02/temperature/main.cpp
@@ -9,8 +9,8 @@ int main()
     double temp;
     cin >> temp;
 
-    cout << temp << " degrees Celsius is " << temp *1.8 +32 << " dergrees Fahrenheit" << endl;
-    cout << temp << " degrees Fahrenheit is " << (temp - 32 )/1.8<< " dergrees Celsius" << endl;
+    cout << temp << " degrees Celsius is " << temp *1.8 +32 << " degrees Fahrenheit" << endl;
+    cout << temp << " degrees Fahrenheit is " << (temp - 32 )/1.8<< " degrees Celsius" << endl;
 
 
     return 0;
