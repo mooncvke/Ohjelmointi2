2b7cbd5fd21dbf353de2f1b0521d78eda574b44f
Tuuli Silvennoinen <tuuli.silvennoinen@tuni.fi>
2022-08-30 16:04:57 +0300

    adde boing and zipboing output

--- student/02/number_series_game/main.cpp
+++ student/02/number_series_game/main.cpp
@@ -10,8 +10,14 @@ int main()
 
     int num =1;
     while (num <= amount) {
+        if ( num % 3 == 0 and num % 7 == 0 ) {
+            std::cout << "zip boing" << std::endl;
+        }
         if ( num % 3 == 0) {
             std::cout<<"zip" << std::endl;
+        }
+        if ( num % 7 == 0) {
+            std::cout << "boing" << std::endl;
         }
 
         std::cout<<num<<std::endl;
