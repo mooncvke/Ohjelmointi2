3fe49891c1e3e7f544b21ebc230eaa6e606c122f
Tuuli Silvennoinen <tuuli.silvennoinen@tuni.fi>
2022-08-30 15:38:35 +0300

    added degrees in fahrenheit

---
+++ student/02/temperature/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Enter a temperature: ";
+
+    double temp;
+    cin >> temp;
+
+    cout << temp << " degrees Celsius is " << temp *1.8 +32 << " dergrees Fahrenheit" << endl;
+
+
+    return 0;
+}
