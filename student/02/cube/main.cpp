14dece5513aa92a9d6f03f6ca9d2508a41283771
Tuuli Silvennoinen <tuuli.silvennoinen@tuni.fi>
2022-09-02 15:44:55 +0300

    cube

---
+++ student/02/cube/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Enter a number: " << endl;
+    int num;
+    cin >> num;
+
+    int cube;
+    cube = num * num * num;
+
+    if (cube < 0 or cube / num / num != num) {
+        cout << "Error! The cube of " << num << " is not " << cube << "." << endl;
+    } else {
+        cout << "The cube of " << num << " is " << cube << "." << endl;
+    }
+
+}
