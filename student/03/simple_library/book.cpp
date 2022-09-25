ab3eb6d81700b1e8e7100f2f536139482b6fba33
Tuuli Silvennoinen <tuuli.silvennoinen@tuni.fi>
2022-09-25 14:17:22 +0300

    working program

--- student/03/simple_library/book.cpp
+++ student/03/simple_library/book.cpp
@@ -1,24 +1,59 @@
 #include "book.hh"
 
-Book::Book(string author, string book_name):
-    author_(author), book_name_(book_name) {
+Book::Book(string const &author, string const &book_name):
+    author_(author),
+    book_name_(book_name),
+    available_(true),
+    loan_date_(Date()),
+    due_date_(Date())
+{
+
 }
 
 void Book::print()
 {
+    cout << author_ << " : " << book_name_ << endl;
+    if (available_ ) {
+        cout << " - available" << endl;
+    } else {
+        cout << "- loaned: ";
+        loan_date_.print();
+        cout << "- to be returned: ";
+        due_date_.print();
 
+    }
 }
 
 void Book::loan(Date const &today)
 {
+    if (!available_) {
+        cout << "Already loaned: cannot be loaned" << endl;
+    } else {
+        available_ = false;
+        loan_date_ = today;
+        due_date_ = today;
+        due_date_.advance(28);
+    }
 
 }
 void Book::renew()
 {
+    if (available_) {
+        cout << "Not loaned: cannot be renewed" << endl;
+    } else {
+        due_date_.advance(28);
+    }
 
 }
 
 void Book::give_back()
 {
+    if (available_ ) {
+        cout << "Not loaned: cannot be given back" << endl;
+    } else {
+        available_ = true;
+    }
 
 }
+
+
--- student/03/simple_library/book.hh
+++ student/03/simple_library/book.hh
@@ -2,6 +2,7 @@
 #define BOOK_HH
 #include "date.hh"
 #include <iostream>
+#include <string>
 
 using namespace std;
 
@@ -9,15 +10,20 @@ using namespace std;
 class Book
 {
     public:
-        Book(string author, string book_name);
+        Book(string const &author, string const &book_name);
         void print();
         void loan(Date const &today);
         void renew();
         void give_back();
 
+
     private:
         string author_;
         string book_name_;
+        bool available_;
+        Date loan_date_;
+        Date due_date_;
+
 };
 
 #endif // BOOK_HH
