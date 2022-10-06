52af86818d313472dd489b6db29a816db9238e9d
Tuuli Silvennoinen <tuuli.silvennoinen@tuni.fi>
2022-10-06 14:38:23 +0300

    line_numbers

---
+++ student/05/line_numbers/main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string input_file;
+    string output_file;
+
+    cout << "Input file: ";
+    cin >> input_file;
+    cout << "Output file: ";
+    cin >> output_file;
+
+    string lines[8];
+    ifstream file(input_file);
+    if ( not file ) {
+        cout << "Error! The file " << input_file << " cannot be opened." << endl;
+    } else {
+        ofstream oFile(output_file);
+        int i = 0;
+        string line;
+        while( getline(file, line) ) {
+            ++i;
+
+            oFile << i << " " << line << endl;
+        }
+        file.close();
+        oFile.close();
+    }
+
+
+    return 0;
+}
