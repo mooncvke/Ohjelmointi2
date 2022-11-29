b8f29f186c9685636c4172b1e80a9985012b309f
Tuuli Silvennoinen <tuuli.silvennoinen@tuni.fi>
2022-11-29 12:28:47 +0200

    zoo

---
+++ student/11/zoo/cockatoo.cpp
@@ -0,0 +1,11 @@
+#include "cockatoo.hh"
+
+Cockatoo::Cockatoo()
+{
+
+}
+
+void Cockatoo::sing(std::ostream &output)
+{
+    std::cout << "Kee-ow kee-ow!" << std::endl;
+}
---
+++ student/11/zoo/mammal.cpp
@@ -0,0 +1,11 @@
+#include "mammal.hh"
+
+Mammal::Mammal(): Animal("Kip kop kip kop")
+{
+
+}
+
+void Mammal::suckle(std::ostream &output)
+{
+    std::cout << "Mus mus" << std::endl;
+}
